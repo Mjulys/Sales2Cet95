443cd9460de08198c0a416d6e41e6aa8b3d6d77c
Mônica Santos Costa <monica.costa.27793@formandos.cinel.pt>
2025-07-06 22:58:40 +0100

    Init web project

---
+++ Sales2Cet95.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sales2Cet95.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
